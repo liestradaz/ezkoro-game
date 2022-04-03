32f8032ba92db22a4058ef6b951fcc4d4026dd0a
nash0x <nachoest@gmail.com>
2022-04-02 23:46:17 -0700

    added sprites for first character

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ function runGame(){
     battleGround.backgroundImg.draw()
 
     knight.update()
-  
+    
+    battleGround.frames++
 
 }
 /*
@@ -87,15 +88,78 @@ class Knight extends Character{
     constructor(hp, attack, speed, width, height, x, y){
         super(hp, attack, speed, width, height, x, y)
         this.img = new Image()
-        this.img.src = "../images/idle1.png"
+        //this.img.src = "../images/idle1.png"
+        //this.img.src = "../images/knight.idle.png"
+        this.frameIdx = 0
+        
+        this.state = {
+            current: "walkAndAttack",
+            idle: 
+            {     
+                img: "../images/knight.idle.png",
+                startIdx: 0,
+                endIdx: 12  
+            },
+            walk:
+            {
+                img: "../images/knight-walk.png",
+                startIdx: 0,
+                endIdx: 5
+            },
+            walkAndAttack:
+            {
+                img: "../images/knight-walk.png",
+                startIdx: 6,
+                endIdx: 11
+            },
+            run:
+            {
+                img: "../images/knight-run.png",
+                startIdx: 0,
+                endIdx: 7
+            },
+            runAndAttack:
+            {
+                img: "../images/knight-run.png",
+                startIdx: 8,
+                endIdx: 14
+            },
+            jump:
+            {
+                img: "../images/knight-jump.png",
+                startIdx: 0,
+                endIdx: 7
+            },
+            death:
+            {
+                img: "../images/knight-death.png",
+                startIdx: 0,
+                endIdx: 9
+            }   
+        }
     }
 
     update(){
+        //const ctx = battleGround.context
+        //ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+        if (this.state.current === "death"){
+            this.frameWidth = 256
+            this.frameHeight = 256
+        } else{
+            this.frameWidth = 128
+            this.frameHeight = 128
+        }
+        //this.idle()
         const ctx = battleGround.context
-        ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+        this.img.src=this.state[this.state.current].img
+        ctx.drawImage(this.img, this.frameIdx * this.frameWidth, 0, this.frameWidth, this.frameHeight, this.x, this.y, this.width, this.height)
+
+        
+        if(battleGround.frames % 7 === 0) { this.frameIdx++ }
+        if(this.frameIdx >= this.state[this.state.current].endIdx) { this.frameIdx = this.state[this.state.current].startIdx } 
     }
 }
-
+ 
 
 
 //************ Executions *************/
@@ -106,3 +170,34 @@ battleGround.start()
 
 
 
+
+window.onload = function(){
+
+
+    document.addEventListener("keydown",(event) => {
+        switch(event.key){
+            case "ArrowLeft":
+                knight.x -= 5
+                knight.state.current = "idle"
+                break;
+            case "ArrowRight":
+                knight.x += 5
+                knight.state.current = "run"
+                break;
+            case "ArrowUp":
+                knight.y -= 5
+                knight.state.current = "jump"
+                break;
+            case "ArrowDown":
+                knight.y += 5
+                knight.state.current = "idle"
+                break;
+            case "m":
+                knight.y += 5
+                knight.state.current = "death"
+                break;
+            
+        }
+    })
+
+}
